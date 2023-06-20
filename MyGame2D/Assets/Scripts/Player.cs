0572cbf308e03e13e0f8ce5be61d5232a80f9679
giovannilmaciel <giomaciel1@gmail.com>
2023-06-19 20:33:06 -0400

    Added jump and improve jump mechanics

--- MyGame2D/Assets/Scripts/Player.cs
+++ MyGame2D/Assets/Scripts/Player.cs
@@ -6,19 +6,26 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
     [SerializeField] float runSpeed;
+    [SerializeField] float jumpSpeed;
 
     Rigidbody2D myRigidbody2D;
+    Animator myAnimator;
+    BoxCollider2D myBoxCollider2D;
+    PolygonCollider2D myPlayersFeet;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody2D = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myBoxCollider2D = GetComponent<BoxCollider2D>();
+        myPlayersFeet = GetComponent<PolygonCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Jump();
     }
 
     private void FixedUpdate()
@@ -35,12 +42,35 @@ public class Player : MonoBehaviour
 
         if (moveController > 0)
         {
+            myAnimator.SetBool("running", true);
             transform.eulerAngles = new Vector3(0, 0, 0);
         }
 
         if(moveController < 0)
         {
+            myAnimator.SetBool("running", true);
             transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        if(moveController == 0)
+        {
+            myAnimator.SetBool("running", false);
+            myAnimator.SetBool("idling", true);
+        }
+
+        
+    }
+
+    private void Jump()
+    {
+        if (!myPlayersFeet.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+
+        bool isJumping = CrossPlatformInputManager.GetButtonDown("Jump");
+
+        if (isJumping)
+        {
+            Vector2 jumpVelocity = new Vector2(myRigidbody2D.velocity.x, jumpSpeed);
+            myRigidbody2D.velocity = jumpVelocity;
         }
     }
 
